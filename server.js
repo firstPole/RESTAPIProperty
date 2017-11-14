1e267fb357009dc833396d48e9d325737cbeb8cb
Vimal Patel <vimal.patel@infostretch.com>
2017-11-14 21:11:14 +0530

    changed EC2 instance IP address

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
+
 var express = require('express'),
-app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  Property = require('./api/models/todoListModel'), //created model loading here
-  bodyParser = require('body-parser');
+	app = express(),
+	  port = process.env.PORT || 3000,
+	  mongoose = require('mongoose'),
+	  Property = require('./api/models/todoListModel'), //created model loading here
+	  bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://54.224.133.253:27017/Property');
+ mongoose.Promise = global.Promise;
+ mongoose.connect('mongodb://54.152.44.2:27017/Property');
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-var routes = require('./api/routes/todoListRoutes'); //importing route
-routes(app); //register the route
-
-
-app.listen(port);
-
-
-console.log('todo list RESTful API server started on: ' + port);
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(bodyParser.json());
